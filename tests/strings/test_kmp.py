14d9604c10835f9c08bfc8bc98982d572a720a6d
Alex Svetkin <svetkin@inventos.ru>
2016-08-28 13:20:12 +0300

    more tests

--- tests/strings/test_kmp.py
+++ tests/strings/test_kmp.py
@@ -1,8 +1,16 @@
 import pytest
 import strings.kmp as kmp
 
+# find
 def test_find_empty_text():
   assert kmp.find('random-pattern', '') == -1
 
+def test_find_simple():
+  assert kmp.find('el', 'On recommend tolerably my belonging or am. Mutual has cannot beauty indeed now sussex merely you') == 27
+
+# find_all
 def test_find_all_empty_text():
   assert kmp.find_all('random-pattern', '') == []
+
+def test_find_all_simple():
+  assert kmp.find_all('el', 'On recommend tolerably my belonging or am. Mutual has cannot beauty indeed now sussex merely you') == [27, 89]
